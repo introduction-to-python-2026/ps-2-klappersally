c7df1c8136586c53f5ac54aa5e049282bb50338f
klappersally <klappersally@gmail.com>
2025-10-30 12:03:57 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -18,5 +18,5 @@ def find_mean(num1, num2, num3):
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
+    sigma = (((num1-mean)**2 + (num2-mean)**2 + (num3-mean)**2)/3)**.5
 
